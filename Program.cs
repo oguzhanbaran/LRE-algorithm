62110c56c6eaa19f749f0d745920526217987d0e
Oğuzhan Baran <50147592+oguzhanbaran@users.noreply.github.com>
2019-04-30 20:08:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LREv1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Cümleyi Girin :");
+            String metin = Console.ReadLine();
+            metin = metin.ToUpper();
+            String karakterler = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ.?,;";
+            int[] count = new int[karakterler.Length];
+            for (int i = 0; i < metin.Length; i++)
+            {
+                int index = karakterler.IndexOf(metin[i]);
+                if (index < 0)
+                    continue;
+                else
+                {
+                    count[index]++;
+                }
+            }
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] < 1)
+                    continue;
+                else
+                {
+                    Console.Write(count[i]+""+karakterler[i]);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
